841f13cb4f15a456eb6a7f21f45af700d37aa4ef
Deli-codex <omoleoladele43@gmail.com>
2022-12-09 09:07:54 -0800

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of char: %zu byte(s)\n", sizeof(char));
@@ -12,5 +13,5 @@ int main(void)
     	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
         printf("Size of float: %zu byte(s)\n", sizeof(float));
-		return (0);
+	return (0);
 }
