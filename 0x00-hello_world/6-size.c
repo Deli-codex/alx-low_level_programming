dda5e4f52366df0d9bece02fe460af94e49d77b2
Deli-codex <omoleoladele43@gmail.com>
2022-12-10 00:10:49 -0800

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,12 @@
 
 int main(void)
 {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float d;
+
 	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
     	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
