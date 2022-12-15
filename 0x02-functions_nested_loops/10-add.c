ded1c0512042dff3bb339a26e4687c4935891ad1
Deli-codex <omoleoladele43@gmail.com>
2022-12-14 18:20:25 -0800

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * add - Adds two values
  * @n1: The first number to be computed
@@ -10,5 +11,7 @@
 int add(int n1, int n2)
 
 {
-	return (n1 + n2)
+
+	return (n1 + n2);
+
 }
