b6316ba53b81e5db1fca5ee8d3f3998d215abfc8
Deli-codex <omoleoladele43@gmail.com>
2022-12-20 11:12:41 -0800

    initial commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _puts - Prints a string
  * @str: The string to print
  * Return: void
  */
+
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
