5ec345019a4e957a4d89527e75df6541d6f6f1bb
Deli-codex <omoleoladele43@gmail.com>
2022-12-20 12:03:39 -0800

    intital commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * _puts -prints a string
- * @str: The string to print
- * Return: void
+ *_puts - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * On success: return the number of characters printed
  */
 
 void _puts(char *str)
 {
-	for (; *str != '0'; str++)
-	{
-	_putchar(*str);
-	}
-	_putchar('\n');
+while (*str)
+_putchar(*str++);
+
+_putchar('\n');
 }
